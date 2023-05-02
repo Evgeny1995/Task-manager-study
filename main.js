390d793bff78401a3054f8c18b19e3866256540c
Evgeny1995 <46008208+Evgeny1995@users.noreply.github.com>
2023-05-02 22:08:42 +0300

    task1-1

--- main.js
+++ main.js
@@ -1,161 +1,170 @@
-import './style.css'
+import { getId } from "./utils.js";
+import "./style.css";
 
-let taskList = document.querySelector(".view-task__list");
-let inpText = document.querySelector(".add-task-form__inp");
-let emptyList = document.querySelector(".task-list__empty");
-let btnAddTask = document.querySelector(".add-task-form__btn");
+// let taskList = document.querySelector(".view-task__list");
+// let inpText = document.querySelector(".add-task-form__inp");
+// let emptyList = document.querySelector(".task-list__empty");
+// let btnAddTask = document.querySelector(".add-task-form__btn");
 
-let form = document.querySelector(".add-task__form");
-const daysWeek = [
-  "Воскресенье",
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-];
+// let form = document.querySelector(".add-task__form");
+// const daysWeek = [
+//   "Воскресенье",
+//   "Понедельник",
+//   "Вторник",
+//   "Среда",
+//   "Четверг",
+//   "Пятница",
+//   "Суббота",
+// ];
 
-// let  = document.querySelector(".")
-//Функция добавляет 0 к дате
-function addLeadingZero(d) {
-  return d < 10 ? "0" + d : d;
-}
-//Функция парсит new Date,в необходимый вид
-function getTaskTimeNow(t = new Date()) {
-  let getYear = t.getFullYear();
-  let getMonth = addLeadingZero(t.getMonth() + 1);
-  let getDay = addLeadingZero(t.getDate());
-  let getDaysOfTheWeeks = daysWeek[t.getDay()];
-  let getHours = addLeadingZero(t.getHours());
-  let getMinutes = addLeadingZero(t.getMinutes());
-  return `${getHours}:${getMinutes} ${getDaysOfTheWeeks}<br> 
-          ${getDay}.${getMonth}.${getYear}`;
-}
-
-let listItem = document.querySelector(".task-list__item");
-let deleteBtn = document.querySelector(".list-item__delete-btn");
-let deliteIcon = document.querySelector(".delete-btn__icon");
-
-form.addEventListener("submit", addTasks);
-
-function addTasks (event) {
-  event.preventDefault();
-  let valueAddtext = inpText.value;
-  if (valueAddtext === "") {
-    return;
-  }
-  let taskHtml = `<li class="task-list__item list-item">
-    <p class="list-item__text font-style">${valueAddtext}</p>
-    <div class="list-item__time font-style__small">${getTaskTimeNow()}</div>
-    <button class="list-item__edit-btn edit-btn" data-action="edit" type="button">
-        <svg class="edit-btn__icon" version="1.0" xmlns="http://www.w3.org/2000/svg" width="512.000000pt" height="512.000000pt"
-            viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
-            <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#7764A9"
-                stroke="none">
-                <path d="M4107 4670 c-32 -12 -77 -32 -100 -47 -23 -14 -181 -164 -352 -334
-                   l-310 -309 318 -317 318 -318 313 315 c293 295 315 320 352 394 38 79 39 82
-                   39 190 -1 91 -5 121 -23 166 -44 111 -142 209 -252 252 -81 31 -226 35 -303 8z" />
-                <path d="M2442 3077 c-424 -424 -772 -777 -772 -782 0 -13 612 -625 625 -625
-                   13 0 1555 1542 1555 1555 0 13 -612 625 -625 625 -6 0 -358 -348 -783 -773z" />
-                <path d="M743 3765 c-124 -34 -213 -108 -270 -223 l-38 -76 0 -1360 0 -1361
-                   23 -58 c34 -82 125 -178 211 -220 l66 -32 1366 0 1365 0 76 38 c121 59 204
-                   167 228 296 6 34 10 387 10 953 l0 900 -29 29 c-38 37 -82 39 -116 4 l-25 -24
-                   0 -904 c0 -991 2 -965 -60 -1039 -16 -20 -53 -48 -82 -62 l-52 -26 -1305 0
-                   c-908 0 -1318 3 -1345 11 -53 15 -138 100 -154 155 -9 32 -12 350 -12 1345 l0
-                   1304 24 51 c13 28 41 65 62 82 78 65 36 62 1034 62 l907 0 27 26 c35 36 34 77
-                   -3 115 l-29 29 -914 -1 c-754 0 -922 -3 -965 -14z" />
-                <path d="M1432 1773 c-73 -208 -133 -386 -132 -396 0 -25 54 -77 79 -77 16 0
-                   759 255 768 264 1 1 -129 133 -290 294 l-292 292 -133 -377z" />
-            </g>
-        </svg>
-    </button>
-    <button class="list-item__delete-btn delete-btn" data-action="delete"  type="button">
-        <svg class="delete-btn__icon" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M18.7782 20.8597C19.364 21.4455 20.3137 21.4455 20.8995 20.8597C21.4853 20.2739 21.4853 19.3241 20.8995 18.7383L13.1213 10.9601L20.8995 3.18201C21.4853 2.59622 21.4853 1.64648 20.8995 1.06069C20.3137 0.474904 19.3639 0.474904 18.7781 1.06069L11 8.83883L3.22185 1.06068C2.63606 0.474891 1.68632 0.474891 1.10053 1.06068C0.514745 1.64646 0.514745 2.59621 1.10053 3.182L8.87868 10.9601L1.10047 18.7384C0.514686 19.3241 0.514686 20.2739 1.10047 20.8597C1.68626 21.4455 2.63601 21.4455 3.22179 20.8597L11 13.0815L18.7782 20.8597Z"
-                fill="#75639F" fill-opacity="0.87" />
-        </svg>
-    </button>
-    </li>`;
-  taskList.insertAdjacentHTML("beforeend", taskHtml);
-  inpText.value = "";
-  if (taskList.children.length > 1) {
-    emptyList.classList.add("hide");
-  }
- 
-}
-
-taskList.addEventListener("click", deleteEditTask);
-
-function deleteEditTask (event) {
-  let editBtn = document.querySelector(".list-item__edit-btn");
-  if(event.target.dataset.action === "delete") {
-    let parentNode = event.target.closest(".task-list__item");
-    parentNode.remove();
-  }
-  if(event.target == editBtn) {
-    event.target.setAttribute("contenteditable", "true");
-    console.log(true)
-  }
-  
-}
-
-
-// document.querySelector(".list-item__text").oninput = function() {
-//   alert(this.innerText);
+// // let  = document.querySelector(".")
+// //Функция добавляет 0 к дате
+// function addLeadingZero(d) {
+//   return d < 10 ? "0" + d : d;
+// }
+// //Функция парсит new Date,в необходимый вид
+// function getTaskTimeNow(t = new Date()) {
+//   let getYear = t.getFullYear();
+//   let getMonth = addLeadingZero(t.getMonth() + 1);
+//   let getDay = addLeadingZero(t.getDate());
+//   let getDaysOfTheWeeks = daysWeek[t.getDay()];
+//   let getHours = addLeadingZero(t.getHours());
+//   let getMinutes = addLeadingZero(t.getMinutes());
+//   return `${getHours}:${getMinutes} ${getDaysOfTheWeeks}<br>
+//           ${getDay}.${getMonth}.${getYear}`;
 // }
 
+// let listItem = document.querySelector(".task-list__item");
+// let deleteBtn = document.querySelector(".list-item__delete-btn");
+// let deliteIcon = document.querySelector(".delete-btn__icon");
 
+// form.addEventListener("submit", addTasks);
 
-
-
-
-
-// taskList.insertAdjacentHTML('afterend',`)
-
-
-// taskList.addEventListener("click", function (event) {
-//   if (event.classList == listItem || deleteBtn) {
-//     listItem.parentNode.removeChild(listItem);
-//   } else {
-//     console.log("false");
+// function addTasks (event) {
+//   event.preventDefault();
+//   let valueAddtext = inpText.value;
+//   if (valueAddtext === "") {
+//     return;
 //   }
-//   console.log(event);
-// });
+//   let taskHtml = `<li class="task-list__item list-item">
+//     <p class="list-item__text font-style">${valueAddtext}</p>
+//     <div class="list-item__time font-style__small">${getTaskTimeNow()}</div>
+//     <button class="list-item__edit-btn edit-btn" data-action="edit" type="button">
+//         <svg class="edit-btn__icon" version="1.0" xmlns="http://www.w3.org/2000/svg" width="512.000000pt" height="512.000000pt"
+//             viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
+//             <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#7764A9"
+//                 stroke="none">
+//                 <path d="M4107 4670 c-32 -12 -77 -32 -100 -47 -23 -14 -181 -164 -352 -334
+//                    l-310 -309 318 -317 318 -318 313 315 c293 295 315 320 352 394 38 79 39 82
+//                    39 190 -1 91 -5 121 -23 166 -44 111 -142 209 -252 252 -81 31 -226 35 -303 8z" />
+//                 <path d="M2442 3077 c-424 -424 -772 -777 -772 -782 0 -13 612 -625 625 -625
+//                    13 0 1555 1542 1555 1555 0 13 -612 625 -625 625 -6 0 -358 -348 -783 -773z" />
+//                 <path d="M743 3765 c-124 -34 -213 -108 -270 -223 l-38 -76 0 -1360 0 -1361
+//                    23 -58 c34 -82 125 -178 211 -220 l66 -32 1366 0 1365 0 76 38 c121 59 204
+//                    167 228 296 6 34 10 387 10 953 l0 900 -29 29 c-38 37 -82 39 -116 4 l-25 -24
+//                    0 -904 c0 -991 2 -965 -60 -1039 -16 -20 -53 -48 -82 -62 l-52 -26 -1305 0
+//                    c-908 0 -1318 3 -1345 11 -53 15 -138 100 -154 155 -9 32 -12 350 -12 1345 l0
+//                    1304 24 51 c13 28 41 65 62 82 78 65 36 62 1034 62 l907 0 27 26 c35 36 34 77
+//                    -3 115 l-29 29 -914 -1 c-754 0 -922 -3 -965 -14z" />
+//                 <path d="M1432 1773 c-73 -208 -133 -386 -132 -396 0 -25 54 -77 79 -77 16 0
+//                    759 255 768 264 1 1 -129 133 -290 294 l-292 292 -133 -377z" />
+//             </g>
+//         </svg>
+//     </button>
+//     <button class="list-item__delete-btn delete-btn" data-action="delete"  type="button">
+//         <svg class="delete-btn__icon" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+//             <path fill-rule="evenodd" clip-rule="evenodd"
+//                 d="M18.7782 20.8597C19.364 21.4455 20.3137 21.4455 20.8995 20.8597C21.4853 20.2739 21.4853 19.3241 20.8995 18.7383L13.1213 10.9601L20.8995 3.18201C21.4853 2.59622 21.4853 1.64648 20.8995 1.06069C20.3137 0.474904 19.3639 0.474904 18.7781 1.06069L11 8.83883L3.22185 1.06068C2.63606 0.474891 1.68632 0.474891 1.10053 1.06068C0.514745 1.64646 0.514745 2.59621 1.10053 3.182L8.87868 10.9601L1.10047 18.7384C0.514686 19.3241 0.514686 20.2739 1.10047 20.8597C1.68626 21.4455 2.63601 21.4455 3.22179 20.8597L11 13.0815L18.7782 20.8597Z"
+//                 fill="#75639F" fill-opacity="0.87" />
+//         </svg>
+//     </button>
+//     </li>`;
+//   taskList.insertAdjacentHTML("beforeend", taskHtml);
+//   inpText.value = "";
+//   if (taskList.children.length > 1) {
+//     emptyList.classList.add("hide");
+//   }
 
+// }
 
+// taskList.addEventListener("click", deleteEditTask);
 
+// function deleteEditTask (event) {
+//   let editBtn = document.querySelector(".list-item__edit-btn");
+//   if(event.target.dataset.action === "delete") {
+//     let parentNode = event.target.closest(".task-list__item");
+//     parentNode.remove();
+//   }
+//   if(event.target == editBtn) {
+//     event.target.setAttribute("contenteditable", "true");
+//     console.log(true)
+//   }
 
+// }
 
+let $rowBottom = document.querySelector(".row-bottom");
+let $inp = document.querySelector(".inp");
+let $btn = document.querySelector(".btn");
+let $btnDel = document.querySelectorAll(".btn-del");
 
+const tasks = [
+  {
+    id: "123",
+    name: "Task1",
+    date: Date.now(),
+  },
+  {
+    id: "1234",
+    name: "Task2",
+    date: Date.now(),
+  },
+  {
+    id: "1235",
+    name: "Task3",
+    date: Date.now(),
+  },
+];
 
+function addToArr(el) {
+  tasks.push({
+    id: getId(),
+    name: el.value,
+    date: Date.now(),
+  });
+}
 
+function renderItem(item) {
+  return `<li class="item">
+            <p class="name">${item.name}</p>
+            <p class="data-id">${item.id}</p>
+            <p class="date">${item.date}</p>
+            <button class="del-btn" type="button">Удалить</button>
+          </li>`;
+}
 
+function renderList(items) {
+  return `<ul class="list">
+          ${items.map((item) => renderItem(item)).join("")}
+          </ul>`;
+}
 
+function addToDOM(target, element) {
+  target.innerHTML = "";
+  target.insertAdjacentHTML("beforeend", element);
+}
 
+function startApp() {
+  const renderedList = renderList(tasks);
 
+  $btn.addEventListener("click", () => {
+    addToArr($inp);
 
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+    const updatedList = renderList(tasks);
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+    addToDOM($rowBottom, updatedList);
+  });
 
-// setupCounter(document.querySelector('#counter'))
+  addToDOM($rowBottom, renderedList);
+}
+
+startApp();
---
+++ utils.js
@@ -0,0 +1,9 @@
+export function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+export function getId() {
+  return randomInteger(1, 999);
+}
